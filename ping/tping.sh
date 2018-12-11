190058757c9cfc74938266c80c4fe50a182f3146
fishba <fishba@qq.com>
2018-12-11 15:47:49 +0800

    new

--- ping/tping.sh
+++ ping/tping.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 #需求是PING一个地址，延迟高于100ms就发送个邮件， 然后正常后在发个邮件通知 之后正常不发。
 function tping(){
-	if [ ! -e /opt/test/$1 ];then
-		echo "0" >/opt/test/${1}
-		n=`cat /opt/test/${1}`
+	PWD=`pwd`/$1
+	if [ ! -e $PWD ];then
+		echo "0" >$PWD
+		n=`cat $PWD`
 	else
-		n=`cat /opt/test/${1}`
+		n=`cat $PWD`
 	fi
-	#echo $n
+	
+	echo $n  $PWD
 	ip=`ping -c 3 -W 1  $1 |awk -F/ 'END{print $6}'`
 	if [ "$ip" == "" ];then
 		if [ "$n" == "0" ];then
-			# echo $n
+			echo $n
 			echo ${1} "不通"
-			/opt/test/ding.sh "${2}"
-			echo "1" >/opt/test/${1}
+			#`pwd`/ding.sh "${2}"
+			echo "1" >$PWD
 		fi
 	elif [ "$n" == "1" ];then
 		echo "${2} 已经恢复正常。"
-		echo "0" >/opt/test/${1}
+		echo "0" >$PWD
 	fi
 
 }
